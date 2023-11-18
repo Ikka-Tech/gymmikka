c6fe718a6f3830c2ecec9da78faa66a055a4f77b
Yaseen <121yaseen@gmail.com>
2023-11-18 19:21:18 +0530

    comfigure twilio

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import assistantRoutes from "./routes/assistant";
 import statusRoute from "./routes/status";
 import gymRoutes from "./routes/gym";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", assistantRoutes);
 app.use("/status", statusRoute);
